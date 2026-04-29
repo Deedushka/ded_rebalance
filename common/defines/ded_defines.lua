53c890991551865153c9910c91e310b994b08915
Dedushka <nikitas41@yandex.ru>
2026-04-29 16:42:49 +0300

    * State Anti-Air:  * Reduced strat bombing by anti-air.  * Anti-Air now harder destroy  * Cost reduced * Buildings:  * some cost reduce  * some build cant destroyed * Greater Italy now need Cairo, no more Ticino * Greater Italy now have new occupation law * Strategic bomber now need more fuel and command power * EXP max - 999 * Anti-Air cas damage reduce now 0.8 * France capitulate after Paris lost * Soviet get easier claims after purge * Mouth lag fix * Del. France alt path * Buffed some free france focus tree paths * Free France now spawn in Central Africa * Vichy Size after France cap now smaller * Fixed some events:  * Vichy now required  * Player on France can only go to Free France or Vichy

--- common/defines/ded_defines.lua
+++ common/defines/ded_defines.lua
@@ -1,7 +1,61 @@
-    NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100
+    -- Game speed change
+    NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 1000
     NDefines.NGame.LAG_DAYS_FOR_PAUSE = 30
-    NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.25, 0.1, 0.05, 0.0 }
+    NDefines.NGame.GAME_SPEED_SECONDS = { 1000.0, 0.35, 0.2, 0.12, 0.0 }
     NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
     NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
 
-    NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.26
+    -- State Anti-Air change
+    NDefines.NAir.ANTI_AIR_PLANE_DAMAGE_CHANCE = 0.5
+    NDefines.NAir.ANTI_AIR_PLANE_DAMAGE_FACTOR = 0.95
+    NDefines.NAir.STRATEGIC_BOMBING_RAILWAY_PRIORITY_SCALE = 0.0
+    NDefines.NAir.SECONDARY_DAMAGE_STRAT = 0.0
+    NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
+		0.0, -- AIR_SUPERIORITY
+		0.0, -- CAS
+		0.0, -- INTERCEPTION
+		0.1, -- STRATEGIC_BOMBER
+		0.0, -- NAVAL_BOMBER
+		0.0, -- DROP_NUKE
+		0.0, -- PARADROP
+		0.0, -- NAVAL_KAMIKAZE
+        0.0, -- PORT_STRIKE
+		100.0, -- ATTACK_LOGISTICS
+		0.05, -- AIR_SUPPLY
+		0.0, -- TRAINING
+		0.0, -- NAVAL_MINES_PLANTING
+		0.0, -- NAVAL_MINES_SWEEPING
+		0.0, -- RECON
+		0.0, -- NAVAL_PATROL
+		0,0, -- BARRAGE
+		0,0, -- SAM
+	}
+    NDefines.NAir.MISSION_FUEL_COSTS = {  -- fuel cost per plane for each mission
+		1.0, -- AIR_SUPERIORITY
+		1.0, -- CAS
+		0.2, -- INTERCEPTION
+		3.0, -- STRATEGIC_BOMBER
+		1.0, -- NAVAL_BOMBER
+		1.0, -- DROP_NUKE
+		1.0, -- PARADROP
+		0.75, -- NAVAL_KAMIKAZE
+		1.2, -- PORT_STRIKE
+		2.0, -- ATTACK_LOGISTICS
+		1.0, -- AIR_SUPPLY
+		0.6, -- TRAINING
+		1.0, -- NAVAL_MINES_PLANTING
+		1.0, -- NAVAL_MINES_SWEEPING
+		1.0, -- RECON
+		1.0, -- NAVAL_PATROL
+		0.0, -- BARRAGE
+		0,0, -- NUCLEAR
+		0,0, -- SAM
+	}
+
+    -- Ground Anti-Air reduce CAS damage
+    NDefines.NAir.ANTI_AIR_MAXIMUM_DAMAGE_REDUCTION_FACTOR = 0.8
+
+    -- EXP change
+    NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999
+    NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999
+    NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999
